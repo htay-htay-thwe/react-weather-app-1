95cff40928c3090be8c45e5faf42237dbf13dd0f
htay-htay-thwe <htayhtaythwe962@gmail.com>
2023-05-12 07:25:22 +0630

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
+import ReactAnimatedWeather from "react-animated-weather";
 
 function App() {
   return (
     <div className="App">
       <h1>Myanmar</h1>
+      <p className="date">
+        {" "}
+        Thursday,6:30
+        <br />
+        Clear
+      </p>
+      <div className="condition">
+        <span className="icon">
+          {" "}
+          icon: 'CLEAR_DAY', color: 'goldenrod', size: 512, animate: true
+        </span>
+        <span className="degree">27°C</span>
+      </div>
+      <ul>
+        <li>Precipitation : 66%</li>
+        <li>Wind : 17km/h</li>
+      </ul>
     </div>
   );
 }
